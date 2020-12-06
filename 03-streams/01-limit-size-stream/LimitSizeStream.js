d96094a0168f709aff151038e07ae3c2fd084706
prochorsimon <Never7umer>
2020-12-07 00:23:29 +0200

    task 3

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -4,10 +4,18 @@ const LimitExceededError = require('./LimitExceededError');
 class LimitSizeStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.limit = options.limit;
+    this.passed = 0;
   }
 
   _transform(chunk, encoding, callback) {
+    this.passed += chunk.length;
 
+    if (this.passed > this.limit) {
+      callback(new LimitExceededError());
+    } else {
+      callback(null, chunk);
+    }
   }
 }
 
