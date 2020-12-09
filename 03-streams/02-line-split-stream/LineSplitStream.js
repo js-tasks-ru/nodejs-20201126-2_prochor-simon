f544eebe5d6951a5317508a715d89e330e691868
prochorsimon <Never7umer>
2020-12-10 01:04:59 +0200

    add line-split-stream

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -4,12 +4,30 @@ const os = require('os');
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+
+    this.prevLines = '';
   }
 
   _transform(chunk, encoding, callback) {
+    const str = this.prevLines + chunk.toString();
+    const lines = str.split(os.EOL);
+    const isLastLine = !str.endsWith(os.EOL);
+
+    this.prevLines = isLastLine ? lines.pop() : '';
+
+    lines.forEach(line => {
+      this.push(line);
+    });
+
+    callback();
   }
 
   _flush(callback) {
+    if (this.prevLines) {
+      this.push(this.prevLines);
+    }
+
+    callback();
   }
 }
 
