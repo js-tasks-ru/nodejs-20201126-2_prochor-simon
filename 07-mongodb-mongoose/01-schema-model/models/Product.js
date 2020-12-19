f6a308b5fcd000299841739fd9488c30ca3fb065
prochorsimon <Never7umer>
2020-12-19 11:51:38 +0200

    add task 01-01

--- 07-mongodb-mongoose/01-schema-model/models/Product.js
+++ 07-mongodb-mongoose/01-schema-model/models/Product.js
@@ -2,6 +2,32 @@ const mongoose = require('mongoose');
 const connection = require('../libs/connection');
 
 const productSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        index: true,
+        required: true,
+    },
+    description: {
+        type: String,
+        index: true,
+        required: true,
+    },
+    price: {
+        type: Number,
+        index: true,
+        required: true,
+    },
+    category: {
+        type: mongoose.Schema.Types.ObjectID,
+        ref: 'Category',
+        required: true,
+    },
+    subcategory: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'subCategory',
+        required: true,
+    },
+    images: [String],
 });
 
 module.exports = connection.model('Product', productSchema);
