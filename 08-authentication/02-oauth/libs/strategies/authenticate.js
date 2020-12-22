ce4424749527f8cbd1baa454c1a19e2486f0ce53
prochorsimon <Never7umer>
2020-12-22 14:41:55 +0200

    add task 8-2

--- 08-authentication/02-oauth/libs/strategies/authenticate.js
+++ 08-authentication/02-oauth/libs/strategies/authenticate.js
@@ -1,3 +1,21 @@
-module.exports = function authenticate(strategy, email, displayName, done) {
-  done(null, false, `функция аутентификации с помощью ${strategy} не настроена`);
+const User = require('../../models/User');
+
+module.exports = async function authenticate(strategy, email, displayName, done) {
+  let user;
+
+  if (!email) {
+    return done(null, false, 'Не указан email');
+  }
+
+  user = await User.findOne({email});
+
+  if (!user) {
+    user = new User({ email, displayName });
+    const err = user.validateSync();
+    if (err) {
+      return done(err);
+    }
+    await user.save();
+  }
+  done(null, user);
 };
